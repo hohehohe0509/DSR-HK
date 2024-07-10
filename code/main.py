e00cff0a09c7bd7267c99a8a07c6af3595b75928
hohehohe0509 <55575979+hohehohe0509@users.noreply.github.com>
2024-07-10 14:30:12 +0800

    Update main.py

--- code/main.py
+++ code/main.py
@@ -14,7 +14,7 @@ def init_seed(seed=None):
     torch.manual_seed(seed)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', default='Retailrocket', help='dataset name: retailrocket/diginetica/Nowplaying/sample')
+parser.add_argument('--dataset', default='Tmall', help='dataset name: retailrocket/diginetica/Nowplaying/sample')
 parser.add_argument('--epoch', type=int, default=7, help='number of epochs to train for')
 parser.add_argument('--batchSize', type=int, default=100, help='input batch size')
 parser.add_argument('--kg_batch_size', type=int, default=100, help='KG batch size.')
@@ -23,37 +23,28 @@ parser.add_argument('--kg_embSize', type=int, default=112, help='embedding size'
 parser.add_argument('--relation_embSize', type=int, default=112, help='Relation Embedding size')
 parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 parser.add_argument('--layer', type=int, default=1, help='the number of layer used')
-parser.add_argument('--beta', type=float, default=0.001, help='ssl task maginitude')
-parser.add_argument('--eps', type=float, default=0.2, help='eps') #這個用不到
-parser.add_argument('--filter', type=bool, default=False, help='filter incidence matrix')#多加的
-parser.add_argument('--lam', type=float, default=0.005, help='diff task maginitude') #這也用不到
+parser.add_argument('--beta', type=float, default=0.05, help='ssl task maginitude')
 parser.add_argument('--layer_size', nargs='?', default='[112, 112, 112]', help='Output sizes of every layer')
 parser.add_argument('--heads', type=int, default=1)
 parser.add_argument('--drop_rate', type=float, default=0.7, help='CL dropout rate.')
-parser.add_argument('--alpha', type=float, default=0.) #這個用不到
+parser.add_argument('--alpha', type=float, default=0.)
 parser.add_argument('--adj_type', nargs='?', default='si',help='Specify the type of the adjacency (laplacian) matrix from {bi, si}.')
 parser.add_argument('--batch_size_cl', type=int, default=8192, help='CL batch size.')
-parser.add_argument('--cl_alpha', type=float, default=1.)#也用不到
-parser.add_argument('--temperature', type=float, default=0.7, help='Softmax temperature.') #也用不到
+parser.add_argument('--temperature', type=float, default=0.1, help='Softmax temperature.')
 parser.add_argument('--K', type=int, default=5, help='')
 parser.add_argument('--seed', type=int, default=2023, help='Random seed used in the experiment')
-# parser.add_argument('--lr_dc', type=float, default=0.1, help='learning rate decay rate')
-# parser.add_argument('--lr_dc_step', type=int, default=2, help='the number of steps after which the learning rate decay 3')
 
 
 opt = parser.parse_args()
 print(opt)
 
 logging.basicConfig(
-    filename='../log/%s_test4.log' % opt.dataset, 
+    filename='../log/%s.log' % opt.dataset, 
     level=logging.INFO, 
     format='%(asctime)s,%(msecs)03d [%(levelname)s] %(name)s: %(message)s',  
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 logging.info(opt)
-
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0,1'
-# torch.cuda.set_device(1)
 
 def main():
     init_seed(opt.seed)
@@ -75,17 +66,13 @@ def main():
     test_data = Data(opt.dataset, test_data,all_train,opt, shuffle=True, n_item=n_item, n_node=n_node, KG=False)
     ret_num = train_data.n_session + train_data.n_items
     
-    ##新加的
+    
     weight_size = eval(opt.layer_size)
     num_layers = len(weight_size) - 2
     heads = [opt.heads] * num_layers + [1]
     adjM = train_data.lap_list
 
-    g = dgl.DGLGraph(adjM)
-    g = dgl.remove_self_loop(g)
-    g = dgl.add_self_loop(g)
-    g = g.to('cuda')
-
+    # create KG
     edge2type = {}
     for i,mat in enumerate(train_data.kg_lap_list):
         for u,v in zip(*mat.nonzero()):
@@ -124,7 +111,7 @@ def main():
         logging.info('epoch: %d'% epoch)
         print('-------------------------------------------------------')
         print('epoch: ', epoch)
-        metrics, total_loss = train_test(model, train_data, test_data, kg, g, epoch, opt.drop_rate)
+        metrics, total_loss = train_test(model, train_data, test_data, kg, epoch, opt.drop_rate)
         for K in top_K:
             metrics['hit%d' % K] = np.mean(metrics['hit%d' % K]) * 100
             metrics['mrr%d' % K] = np.mean(metrics['mrr%d' % K]) * 100
